d5c112a2fff27d34786bc51d875899707ba8c1b4
BalasaRadu <145685329+BalasaRadu@users.noreply.github.com>
2024-10-10 14:42:34 +0300

    Add files via upload

---
+++ javaMaze/starting/field.js
@@ -0,0 +1,112 @@
+const Queue = require('queue-fifo');
+
+const hatCharacter = '^';
+const holeCharacter = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {
+	constructor(height, width, complexity) {
+		this._height = height;
+		this._width = width;
+		this._complexity = complexity - 1;
+		this._playerCoords = [0, 0];
+		this._hatCoords = [height - 1, width - 1];
+		this._hatFound = false;
+
+		this.generateField();
+	}
+	get hatFound() {
+		return this._hatFound;
+	}
+	fillField(startCoord, visited) {
+		const dx = [-1, 1, 0, 0];
+		const dy = [0, 0, -1, 1];
+		
+		const queue = new Queue();
+		queue.enqueue({'x': startCoord[0], 'y': startCoord[1]});
+		visited[startCoord[0]][startCoord[1]] = 1;
+		
+		while(queue.size()) {
+			const x = queue.peek().x;
+			const y = queue.peek().y;
+			queue.dequeue();
+			for (let l = 0; l < 4; l++) {
+				const newX = x + dx[l];
+				const newY = y + dy[l]; 
+				if (this.checkPosition([newX, newY]) && visited[newX][newY] == 0) {
+					visited[newX][newY] = visited[x][y] + 1;
+					queue.enqueue({'x': newX, 'y': newY});
+				}
+			}
+		}
+	}
+	checkField() {
+		let visited = Array.from({length: this._height}, () => Array.from({length: this._height}).fill(0));
+		this.fillField(this._playerCoords, visited);
+		const roadLength = visited[this._hatCoords[0]][this._hatCoords[1]];
+		const optimalLength = Math.floor((this._height + this._width) * (100 + this._complexity * 10) / 100);
+		return roadLength >= optimalLength && roadLength < optimalLength * 12 / 10;
+	}
+	generateField() {
+		do {
+			this._field = Array.from({ length: this._height }, () => 
+				Array.from({ length: this._width }, () => {
+					const seed = Math.random() < 0.9 - this._complexity / 10;
+					return seed ? fieldCharacter : holeCharacter;
+				})
+			);
+
+			this._field[this._playerCoords[0]][this._playerCoords[1]] = pathCharacter;
+			this._field[this._height - 1][this._width - 1] = hatCharacter;
+
+		} while(!this.checkField());
+	}
+	checkPosition(coords) {
+		return coords[0] >= 0 &&
+				coords[0] < this._height &&
+				coords[1] >= 0 &&
+				coords[1] < this._width &&
+				this._field[coords[0]][coords[1]] != holeCharacter;
+	}
+	updateFiled() {
+		if (!this.checkPosition(this._playerCoords)) {
+			console.log('You lost :(');
+			process.exit();
+		}
+		this._field[this._playerCoords[0]][this._playerCoords[1]] = pathCharacter;
+		if (this._playerCoords[0] == this._hatCoords[0] && this._playerCoords[1] == this._hatCoords[1]) {
+			console.log('You found your hat :)');
+			this._hatFound = true;
+		}
+	}
+	movePlayer(key) {
+		if (key) {
+			if (key.name == 'w') {
+				this._playerCoords[0]--;
+			}
+			if (key.name == 's') {
+				this._playerCoords[0]++;
+			}
+			if (key.name == 'a') {
+				this._playerCoords[1]--;
+			}
+			if (key.name == 'd') {
+				this._playerCoords[1]++;
+			}
+			if (key.name == 'q') {
+				console.log('Quiting the game...');
+				process.exit();
+			}
+		}
+		this.updateFiled();
+	}
+	displayField() {
+		this._field.forEach((element) => {
+			console.log(`${element.join('')}`);
+		});
+		console.log(this._playerCoords);
+	}
+}
+
+module.exports = Field;
---
+++ javaMaze/starting/main.js
@@ -0,0 +1,55 @@
+const keypress = require('keypress');
+const Field = require('./field.js');
+
+function displayGame(field, level) {
+	console.clear();
+	console.log(`Level: ${level}\n`);
+	field.displayField();
+	console.log('Press a, s, d, w to move. Press q to quit.');
+}
+
+function playLevel(level) {
+	return new Promise((resolve) => {
+		const field = new Field(10 + (level - 1) * 2, 10 + (level - 1) * 2, level);
+	 	displayGame(field, level);
+		keypress(process.stdin);
+		process.stdin.on('keypress', (ch, key) => {
+			field.movePlayer(key);
+			displayGame(field, level);
+			if (field.hatFound) {
+				process.stdin.removeAllListeners('keypress');
+				resolve();
+			}
+		});
+	});
+}
+
+function pressEnter() {
+	return new Promise((resolve) => {
+		console.log('Press Enter to continue.');
+		keypress(process.stdin);
+		process.stdin.on('keypress', (ch, key) => {
+			if (key.name === 'return') {
+				process.stdin.removeAllListeners('keypress');
+				resolve();
+			}
+		});
+	});
+}
+
+async function playGame() {
+	for (let currLevel = 1; currLevel <= 5; currLevel++) {
+		await playLevel(currLevel);
+		if (currLevel < 5) {
+			console.log(`Congratulation! You completed level ${currLevel}.`);
+			await pressEnter();
+		}
+	}
+	console.log(`Congratulation! You completed all levels.`);
+	process.exit();
+}
+
+playGame();
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
